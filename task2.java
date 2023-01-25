ca8959509e7ee2ba797e941b995e686c20396a1a
Saglara <nohaevass@yandex.ru>
2023-01-25 20:34:23 +0300

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,15 @@
+// Напишите метод, который сжимает строку. Пример: вход aaaabbbcdd.
+// Вывод: a4b3c1d2 для примера
+
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    System.out.print("Введите строку: ");
+    String str = scan.nextLine();
+    scan.close();
+    
+    }
+    
+}
