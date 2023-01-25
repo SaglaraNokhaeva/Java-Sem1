947e02eb4c64999a633cdc1e084162d2f7635356
Saglara <nohaevass@yandex.ru>
2023-01-25 20:54:52 +0300

    Update task2.java

--- task2.java
+++ task2.java
@@ -3,13 +3,29 @@
 
 import java.util.Scanner;
 
+import javax.lang.model.element.Element;
+
 public class task2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-    System.out.print("Введите строку: ");
-    String str = scan.nextLine();
-    scan.close();
-    
+        System.out.print("Введите строку: ");
+        String string = scan.nextLine();
+        scan.close();
+        System.out.println(ZipStr(string));
+
     }
-    
+
+    public static String ZipStr (String str) {
+                StringBuilder zip= new StringBuilder();
+                int count=1;
+                for (int i=0; i<str.length()-1;i++){
+                   if (str.charAt(i)==(str.charAt(i+1)))  {             
+                    count++;
+                }
+                else {
+                    zip.append(str.charAt(i)).append(Integer.toString(count));
+                    count=1;
+                }
+            }
+                return zip.toString();  
 }
